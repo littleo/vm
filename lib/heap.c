3fc5243249a3d497a84deeaa848caed194d9a931
nicklie <nikpatris@gmail.com>
2020-01-06 23:04:00 +0200

    trying to mege new_stack branch

--- lib/heap.c
+++ lib/heap.c
@@ -4,16 +4,32 @@ void heap_init(heap_s * hp) {
     hp->capacity = 0;
     hp->count = 0;
     hp->data = NULL;
-    hp->free_list = NULL;
+    vector_init(&hp->free_list);
 }
 
 void heap_push(heap_s * hp, stack_e * sp, heap_e entry) {
+    
     if (hp->capacity < hp->count+1) {
-        // No we have problem
+        // 1. call garbage collector and fit empty entries
+        // call_gc();
+        // gc_mark();
+        // gc_sweep();
+        // //
+        if (hp->free_list.count > 0) {
+            uint32_t offset = vector_pop(&hp->free_list);
+            hp->data[offset] = entry;
+        }
+
+        // 2. try to realloc
         uint32_t old_capacity = hp->capacity;
         hp->capacity = GROW_CAPACITY(old_capacity);
         hp->data = GROW_ARRAY(hp->data, heap_e, 
             old_capacity, hp->capacity);
+
+        if (hp->data == NULL) {
+            printf("I CAN'T DO ANYTHING ELSE");
+            exit(1);
+        }
     }
 
     hp->data[hp->count] = entry;
