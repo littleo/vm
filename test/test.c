d8ba91aad8a4a7211eeb9a0d83866d5ee55a8518
nicklie <nikpatris@gmail.com>
2020-01-06 22:58:55 +0200

    new stack struct wasn't tested yet, maybe another time

--- test/test.c
+++ test/test.c
@@ -20,11 +20,15 @@ void printbits(void * pnt0, size_t cnt) {
 
 int main(int argc, char const *argv[])
 {
-    stack_e * stack = (stack_e *) malloc(sizeof(stack_e) * STACK_SIZE);
+    stack_e * stack_base, * sp;
+    stack_base = (stack_e *) malloc(sizeof(stack_e) * STACK_SIZE);
+   
+    heap_e * heap_base;
     heap_s heap;
     heap_init(&heap);
 
-    
+	stack_e default_stack_val = {0,0,0};
+	stack_e default_stack_ptr = {0,1,0};
 
     free(stack);
 
